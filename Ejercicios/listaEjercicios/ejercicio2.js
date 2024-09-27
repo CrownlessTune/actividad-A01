c9dbf1043d308c5ff4dc2b76b2f168c32fe4e227
unknown <falvcha2003@g.educaand.es>
2024-09-27 12:33:34 +0200

    ejercicio 2 hecho

---
+++ Ejercicios/listaEjercicios/ejercicio2.js
@@ -0,0 +1,7 @@
+// Cambiamos el console log a un alert para que se muestre por pantalla y no por consola
+
+alert("hola mundo");
+
+// Y añadimos otro alert para el segundo mensaje
+
+alert("soy el primer script");
