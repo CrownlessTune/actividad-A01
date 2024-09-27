6e6ebc1c0bc7bcdd827c090445fb809a9fc6ad5b
unknown <falvcha2003@g.educaand.es>
2024-09-27 10:58:15 +0200

    plantilla

---
+++ Ejercicios/listaEjercicios/ejercicio1.js
@@ -0,0 +1,20 @@
+// Ejercicio 1: Variables y Tipos de Datos
+
+// Declara tres variables, una para cada tipo de dato primitivo que hemos visto (cadena, número y booleano). Asigna valores a las variables y, usando la consola, muestra el tipo de cada una con typeof.
+// Realiza una conversión de tipo para una de las variables, por ejemplo, convierte un número a cadena.
+
+let cadena = "Ramen"
+
+let numero = 115
+
+let buleano = false
+
+
+console.log("variable cadena: ", typeof(cadena)," / variable número:", typeof(numero)," / variable buleano: ", typeof(buleano));
+
+
+cadena = "245"
+
+console.log(typeof(parseInt(cadena)))
+
+console.log(typeof((numero.toString())))
